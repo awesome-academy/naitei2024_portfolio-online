213e5d0d4292b5989cae4d009a84667f7d0b34df
Ho Tan Anh <anh152k3@gmail.com>
2024-07-30 14:49:48 +0700

    Login-UI Login-Function

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,13 +1,10 @@
 import express, { Request, Response, NextFunction } from 'express'
-import { loginPost, registerPost, showLoginForm, showRegisterForm } from '~/controllers/auth.controller'
+import authRoute from './auth/auth.router'
 const router = express.Router()
 
 /* GET home page. */
 router.get('/', function (req: Request, res: Response, next: NextFunction) {
   res.render('index', { title: 'Express' })
 })
-router.get('/register', showRegisterForm)
-router.post('/register', registerPost)
-router.get('/login', showLoginForm)
-router.post('/login', loginPost)
+router.use('/auth', authRoute)
 export default router
