9a9cc65e66608f563b0b307ac80c92927140f0a6
Huỳnh Hải Đăng <huynh.hai.dang@sun-asterisk.com>
2024-08-13 10:36:29 +0700

    Edit + Add project

--- src/api_service/cloudinary.service.ts
+++ src/api_service/cloudinary.service.ts
@@ -1,9 +1,6 @@
 import dotenv from 'dotenv'
-import multer, { Multer } from 'multer'
 import { v2 as cloudinary, UploadApiResponse, UploadApiErrorResponse } from 'cloudinary'
 import sharp from 'sharp'
-import { NextFunction, Request, Response } from 'express'
-import { CLOUDINARY_NUM } from '~/constants/cloudinary.constant'
 
 dotenv.config()
 
@@ -13,69 +10,33 @@ cloudinary.config({
   api_secret: process.env.CLOUDINARY_SECRET
 })
 
-interface CloudinaryFile extends Express.Multer.File {
-  buffer: Buffer
+const createUploadPromise: (buffer: Buffer, folder: string) => Promise<UploadApiResponse> = (buffer, folder) => {
+  return new Promise((resolve, reject) => {
+    const uploadStream = cloudinary.uploader.upload_stream(
+      {
+        resource_type: 'auto',
+        folder: folder
+      },
+      (err: UploadApiErrorResponse | undefined, result: UploadApiResponse | undefined) => {
+        if (err) {
+          return reject(err)
+        }
+        if (!result) {
+          return reject(new Error('Cloudinary upload result is undefined'))
+        }
+        return resolve(result)
+      }
+    )
+    uploadStream.end(buffer)
+  })
 }
 
-const storage = multer.memoryStorage()
-
-export const upload: Multer = multer({
-  storage: storage,
-  limits: { fileSize: CLOUDINARY_NUM.MAX_FILE_SIZE },
-  fileFilter: (req, file, cb) => {
-    if (file.size > CLOUDINARY_NUM.MAX_FILE_SIZE) {
-      return cb(null, false)
-    }
-    cb(null, true)
+export async function uploadFile(file: Express.Multer.File | undefined, folder: string = 'misc') {
+  if (!file) {
+    throw new Error('No file provided')
   }
-})
-
-export const uploadToCloudinary = async (req: Request, res: Response, next: NextFunction) => {
-  try {
-    let files: CloudinaryFile[] = []
-
-    if (Array.isArray(req.files)) {
-      files = req.files as CloudinaryFile[]
-    } else if (req.files && typeof req.files === 'object') {
-      for (const key in req.files) {
-        if (Object.prototype.hasOwnProperty.call(req.files, key)) {
-          const fileArray = req.files[key] as CloudinaryFile[]
-          files.push(...fileArray)
-        }
-      }
-    }
-
-    if (files.length === 0) {
-      return next(new Error('No files provided'))
-    }
-
-    const cloudinaryUrls: string[] = []
-    for (const file of files) {
-      const resizedBuffer: Buffer = await sharp(file.buffer).resize({ width: 800, height: 600 }).toBuffer()
-
-      const uploadStream = cloudinary.uploader.upload_stream(
-        {
-          resource_type: 'auto',
-          folder: 'your-cloudinary-folder-name'
-        } as any,
-        (err: UploadApiErrorResponse | undefined, result: UploadApiResponse | undefined) => {
-          if (err) {
-            return next(err)
-          }
-          if (!result) {
-            return next(new Error('Cloudinary upload result is undefined'))
-          }
-          cloudinaryUrls.push(result.secure_url)
-
-          if (cloudinaryUrls.length === files.length) {
-            req.body.cloudinaryUrls = cloudinaryUrls
-            next()
-          }
-        }
-      )
-      uploadStream.end(resizedBuffer)
-    }
-  } catch (error) {
-    return next(error)
-  }
+  const resizedBuffer = await sharp(file.buffer).resize({ width: 800, height: 600 }).toBuffer()
+  const promise = createUploadPromise(resizedBuffer, folder)
+  const result = await promise
+  return result.secure_url
 }
