e51839eaead3d17e1643b667437a5f26a4b2e99c
Huỳnh Hải Đăng <huynh.hai.dang@sun-asterisk.com>
2024-08-21 17:28:49 +0700

    searchbar+hide-navbar

--- src/router/index.ts
+++ src/router/index.ts
@@ -9,9 +9,15 @@ import blogRoute from './blog/blog.router'
 import guestRoute from './guest/guest.router'
 const router = express.Router()
 
+router.use((req, res, next) => {
+  res.locals.session = req.session
+  next()
+})
+
 /* GET home page. */
 router.get('/', function (req: Request, res: Response, next: NextFunction) {
   res.render('index')
+  console.log(req.session.user)
 })
 router.get('/register', showRegisterForm)
 router.post('/register', registerPost)
